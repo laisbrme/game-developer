9c268fc4989b6500376ea3eecba312fe03f62e97
Laís Brum Menezes <laisbrme@gmail.com>
2025-04-02 02:22:22 -0300

    :construction: feat(collision): implementa blocos de colisão no jogo

---
+++ game/assets/js/classes/CollisionBlock.js
@@ -0,0 +1,17 @@
+// Definição de blocos de colisão (imagens) para o jogo:
+class CollisionBlock {
+    constructor({ position }) {
+        this.position = position; // Posição do sprite (objeto com propriedades x e y)
+        this.width = 16; // Largura do bloco de colisão
+        this.height = 16; // Altura do bloco de colisão
+    }
+
+    draw() { // Método para desenhar o bloco de colisão no canvas
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'; // Cor do bloco de colisão (transparente)
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height); // Desenha o retângulo do bloco de colisão
+    }
+
+    update() { // Método para atualizar o sprite
+        this.draw(); // Chama o método draw para desenhar o sprite
+    }
+}
