1ea3b9bb32e91d4f48e46ae74a853e7848adbc0d
Laís Brum Menezes <laisbrme@gmail.com>
2025-04-02 14:02:01 -0300

    :package: chore(utils): adiciona arquivo utils.js para funções utilitárias

---
+++ game/assets/js/utils.js
@@ -0,0 +1,11 @@
+function collision({
+    object1, // O primeiro objeto a verificar a colisão
+    object2 // O segundo objeto a verificar a colisão
+}) {
+    return (
+        object1.position.y + object1.height >= object2.position.y && // Verifica se um objeto está acima do bloco de colisão
+        object1.position.y <= object2.position.y + object2.height && // Verifica se um objeto está abaixo do bloco de colisão
+        object1.position.x <= object2.position.x + object2.width && // Verifica se um objeto está à esquerda do bloco de colisão
+        object1.position.x + object1.width >= object2.position.x // Verifica se um objeto está à direita do bloco de colisão
+    )
+}
